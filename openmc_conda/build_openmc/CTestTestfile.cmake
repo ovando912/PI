ae2f0081104332fdaf3c8612efd5e34a2e34b7db
ovando912 <131219367+ovando912@users.noreply.github.com>
2024-11-21 20:33:14 -0300

    Init

--- openmc_conda/build_openmc/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lucas/Proyecto_Integrador/openmc_conda/openmc
-# Build directory: /home/lucas/Proyecto_Integrador/openmc_conda/build_openmc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("vendor/Catch2")
-subdirs("tests/cpp_unit_tests")
--- openmc_conda/build_openmc/vendor/Catch2/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
+++
@@ -1,4 +0,0 @@
-vendor/Catch2/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
- /home/lucas/Proyecto_Integrador/openmc_conda/openmc/vendor/Catch2/src/catch2/internal/catch_result_type.cpp \
- /usr/include/stdc-predef.h \
- /home/lucas/Proyecto_Integrador/openmc_conda/openmc/vendor/Catch2/src/catch2/../catch2/internal/catch_result_type.hpp
